729c306d684259707bb3df18748ecc63ef2c99a9
MykhailovMisha <mihalov.misha@gmail.com>
2020-04-22 01:57:59 +0300

    Updates

---
+++ precache-manifest.2aedc2bcd7f009e6d70932bbfce3a088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2738843b1edf4084bfc8dd4fc2f91c3b",
+    "url": "/MykhailovMisha/covid-19/index.html"
+  },
+  {
+    "revision": "b150343f7f90a408cf89",
+    "url": "/MykhailovMisha/covid-19/static/css/main.8a4c09d7.chunk.css"
+  },
+  {
+    "revision": "f8c7e8e8e6ffc6b7053a",
+    "url": "/MykhailovMisha/covid-19/static/js/2.f3a67142.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/MykhailovMisha/covid-19/static/js/2.f3a67142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b150343f7f90a408cf89",
+    "url": "/MykhailovMisha/covid-19/static/js/main.8a8f9202.chunk.js"
+  },
+  {
+    "revision": "a2dd91716e39c8efea3e",
+    "url": "/MykhailovMisha/covid-19/static/js/runtime-main.c3c5dbde.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/MykhailovMisha/covid-19/static/media/covid.d7265326.png"
+  }
+]);
